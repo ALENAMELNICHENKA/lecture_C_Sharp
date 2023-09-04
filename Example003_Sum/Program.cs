ec4d5fed115bc1febbb797f2e17507394696081c
Alena Melnichenka <6983314@mail.ru>
2023-09-04 22:17:49 +0300

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Определим переменную
+int numberA = 31;
+int numberB = 5;
+//можно добавить сохранение в промежуточную переменную
+int result = numberA + numberB;
+//Выводим инфу в консоль
+Console.WriteLine(result);
