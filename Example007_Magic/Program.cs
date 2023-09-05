0ed2f28183757073861284774c4370f309d302b0
Alena Melnichenka <6983314@mail.ru>
2023-09-05 20:20:04 +0300

    7

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+//Определим вершину треугольника
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 4;
+int xc = 12;
+int yc = 4;
+
+// то же можно записать  и так:
+// int xa = 1, ya = 1,
+//    xb = 1, yb = 30,
+//    xc = 40, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
